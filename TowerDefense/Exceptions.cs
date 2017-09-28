b974d2aaab4123736658aa887b10c41d9a9ff23e
Sam <Sam@DESKTOP-CVEM3OG>
2017-09-28 13:15:16 -0400

    finished

---
+++ TowerDefense/Exceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefense
+{
+    class Exceptions : System.Exception
+    {
+        public Exceptions()
+        {
+
+        }
+
+        public Exceptions(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : Exceptions
+    {
+        public OutOfBoundsException()
+        {
+
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+}
